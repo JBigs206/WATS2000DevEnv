57a8a03e0c8b0989913bc5e03f123b50babb57f5
JBigs206 <JeffreyRichardBigler@gmail.com>
2016-04-19 19:10:20 -0700

    Mage changes to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 // Generated on 2016-03-31 using
 // generator-webapp 1.1.2
-//'use strict';
+'use strict';
 
 // # Globbing
 // for performance reasons we're only matching one level down:
@@ -40,7 +40,7 @@ module.exports = function (grunt) {
       },
       pages: {
         options: {
-          remote: 'git@github.com:JBigs206/WATS2000DevEnv.git',
+          remote: 'https://github.com/JBigs206/WATS2000DevEnv.gitjikuji',
           branch: 'gh-pages'
         }
       }
